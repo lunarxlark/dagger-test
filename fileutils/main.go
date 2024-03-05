cb63b6fbd8207db9b8a207b4753b87b72769a206
lunarxlark <lunarxlark@gmail.com>
2024-03-05 23:38:18 +0900

    fix:modify ExecCommand to simple

--- fileutils/main.go
+++ fileutils/main.go
@@ -7,10 +7,10 @@ import (
 type Fileutils struct{}
 
 func (f *Fileutils) Ls(ctx context.Context, dir *Directory) (string ,error) {
-    return dag.Container().
-        From("alpine:latest").
+	return dag.Container().
+		From("alpine:latest").
 		WithMountedDirectory("/mnt", dir).
 		WithWorkdir("/mnt").
-		WithExec([]string{"ls", "-la"}).
+		WithExec([]string{"ls"}).
 		Stdout(ctx)
 }
